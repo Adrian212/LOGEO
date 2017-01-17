dbb6648581fb0ac7c6a2dec78b2f5400e09c69bb
AdrianFelipeAntonio <adrianf-antonio@hotmail.com>
2017-01-17 15:12:47 -0600

    Update principal.java

--- Herencia/src/herencia/principal.java
+++ Herencia/src/herencia/principal.java
@@ -13,7 +13,7 @@ import javax.swing.JOptionPane;
  *
  * @author Laptop
  */
-//hola mundo
+//inicio
 public final class principal extends javax.swing.JFrame {
     BotonContador botona;
     BotonContador botonb;
