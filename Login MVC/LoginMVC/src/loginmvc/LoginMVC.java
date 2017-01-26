8d9676129709ec05817f6d00d0879daca6e96103
Javier <Javier@Javier-PC>
2017-01-26 16:00:15 -0600

    SESION

---
+++ Login MVC/LoginMVC/src/loginmvc/LoginMVC.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package loginmvc;
+
+import Controlador.ControladorLogin;
+import Modelo.EmpleadoDAO;
+import Vista.JFLogin;
+
+/**
+ *
+ * @author Oscar
+ */
+public class LoginMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFLogin vistaL = new JFLogin();
+        EmpleadoDAO modeloL = new EmpleadoDAO();
+        ControladorLogin controladorL = new ControladorLogin(vistaL, modeloL);
+        vistaL.setVisible(true);
+        vistaL.setLocationRelativeTo(null);
+    }
+    
+}
